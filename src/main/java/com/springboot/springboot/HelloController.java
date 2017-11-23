a484836d408d30bc73fdc4b5ccac0fc7c350e180
TylerYin0928@126.com <Yjf_090214>
2017-11-23 22:15:51 +0800

    how to use controller annotation.

--- src/main/java/com/springboot/springboot/HelloController.java
+++ src/main/java/com/springboot/springboot/HelloController.java
@@ -2,16 +2,16 @@ package com.springboot.springboot;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @Description Spring Boot Demo
  * @Author Tyler Yin
  * @Create 2017-11-22 21:34
  **/
+//RestController相当于同时使用了Controller和ResponseBody两个注解
 @RestController
+@RequestMapping(value="hello")
 public class HelloController {
     @Value("${age}")
     private int age;
@@ -25,9 +25,27 @@ public class HelloController {
     @Autowired
     private BoyProperties boyProperties;
 
-    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    @RequestMapping(value = {"/say", "/hi"}, method = RequestMethod.GET)
     public String demo() {
         return "Hello Spring Boot <p>" + "The girl " + content + "<p> The boy hobby is "
                 + boyProperties.getHobby();
+    }
+
+    @RequestMapping(value="path/{id}")
+    public String pathVariableTest(@PathVariable("id") Integer id){
+        return "id: " + id;
+    }
+
+    /**
+     * 1.采用requestParam这种方式，可以获取Get和Post请求中传递过来的数据
+     * 2.为了简化注解，可以使用GetMapping代替下面注释的注解，两者是等价的
+     * 类似的还有PutMapping等等
+     * @param myId
+     * @return
+     */
+    @GetMapping(value="/param")
+    //@RequestMapping(value="/param", method = RequestMethod.GET)
+    public String requestParamTest(@RequestParam(value = "id", required = false, defaultValue = "100") Integer myId) {
+        return "id: " + myId;
     }
 }
